b1895eba48910be3efe817d784cdebe7e6e691c1
Kevin Grandon <kevingrandon@yahoo.com>
2018-10-29 16:28:29 -0700

    Initial integration testing framework for apollo client

---
+++ app-fixture/src/main.js
@@ -0,0 +1,50 @@
+// @flow
+import Router from 'fusion-plugin-react-router';
+import App, {ApolloClientToken, GraphQLSchemaToken} from 'fusion-apollo';
+import ApolloServer, {
+  ApolloServerEndpointToken,
+} from 'fusion-plugin-apollo-server';
+import {FetchToken} from 'fusion-tokens';
+import unfetch from 'unfetch';
+import {addMockFunctionsToSchema, makeExecutableSchema} from 'graphql-tools';
+
+import ApolloClientPlugin, {
+  ApolloClientEndpointToken,
+  // $FlowFixMe
+} from './pluginCopy'; // eslint-disable-line
+
+import root from './root.js';
+
+const typeDefs = `
+type Query {
+   getHello: Test
+}
+type Test {
+   name: String
+}
+`;
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+});
+
+addMockFunctionsToSchema({schema});
+
+export default () => {
+  const app = new App(root);
+  // eslint-disable-next-line
+  __BROWSER__ && app.register(FetchToken, window.fetch);
+  __NODE__ && app.register(FetchToken, unfetch);
+  app.register(Router);
+
+  app.register(ApolloClientToken, ApolloClientPlugin);
+
+  // Integration tests ensures that we are able to hydrate without hitting this endpoint.
+  // Don't update without updating tests as well.
+  app.register(ApolloClientEndpointToken, '/graphql');
+  __NODE__ && app.register(ApolloServerEndpointToken, '/graphql');
+  __NODE__ && app.register(ApolloServer);
+  __NODE__ && app.register(GraphQLSchemaToken, schema);
+
+  return app;
+};
---
+++ src/__tests__/integration.node.js
@@ -0,0 +1,53 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+/* eslint-env node */
+
+import path from 'path';
+
+import puppeteer from 'puppeteer';
+import {test} from 'fusion-test-utils';
+import {dev} from 'fusion-cli/test/run-command';
+
+test(
+  'able to hydrate GraphQL data from SSR render',
+  async t => {
+    const dir = path.resolve(__dirname, '../../app-fixture');
+    const {proc, port} = await dev(`--dir=${dir}`);
+
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
+    const allUrls = [];
+    page.on('response', response => {
+      const req = response.request();
+      allUrls.push(req.url());
+    });
+
+    await page.goto(`http://localhost:${port}/`, {waitUntil: 'load'});
+
+    const content = await page.content();
+    const expectedResult = {
+      getHello: {name: 'Hello World', __typename: 'Test'},
+    };
+    t.ok(
+      content.includes(JSON.stringify(expectedResult)),
+      'app content contains mock GQL result'
+    );
+
+    t.ok(
+      !allUrls.some(val => val.includes('/graphql')),
+      'did not find any requests to /graphql'
+    );
+
+    await browser.close();
+    proc.kill();
+  },
+  // $FlowFixMe - Need to add timeouts
+  60000
+);
