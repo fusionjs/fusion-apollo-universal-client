53f4e88e7f0d1552258827baa564d8f5bda6254b
Kevin Grandon <kevingrandon@yahoo.com>
2018-11-20 14:39:09 -0800

    Update flow types for flow@0.85
    https://github.com/fusionjs/fusion-apollo-universal-client/pull/165

--- src/__tests__/integration.node.js
+++ src/__tests__/integration.node.js
@@ -13,41 +13,37 @@ import puppeteer from 'puppeteer';
 import {test} from 'fusion-test-utils';
 import {dev} from 'fusion-cli/test/run-command';
 
-test(
-  'able to hydrate GraphQL data from SSR render',
-  async t => {
-    const dir = path.resolve(__dirname, '../../app-fixture');
-    const {proc, port} = await dev(`--dir=${dir}`);
+test('able to hydrate GraphQL data from SSR render', async t => {
+  const dir = path.resolve(__dirname, '../../app-fixture');
+  const {proc, port} = await dev(`--dir=${dir}`);
 
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
-    const allUrls = [];
-    page.on('response', response => {
-      const req = response.request();
-      allUrls.push(req.url());
-    });
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  const allUrls = [];
+  page.on('response', response => {
+    const req = response.request();
+    allUrls.push(req.url());
+  });
 
-    await page.goto(`http://localhost:${port}/`, {waitUntil: 'load'});
+  await page.goto(`http://localhost:${port}/`, {waitUntil: 'load'});
 
-    const content = await page.content();
-    const expectedResult = {
-      getHello: {name: 'Hello World', __typename: 'Test'},
-    };
-    t.ok(
-      content.includes(JSON.stringify(expectedResult)),
-      'app content contains mock GQL result'
-    );
+  const content = await page.content();
+  const expectedResult = {
+    getHello: {name: 'Hello World', __typename: 'Test'},
+  };
+  t.ok(
+    content.includes(JSON.stringify(expectedResult)),
+    'app content contains mock GQL result'
+  );
 
-    t.ok(
-      !allUrls.some(val => val.includes('/graphql')),
-      'did not find any requests to /graphql'
-    );
+  t.ok(
+    !allUrls.some(val => val.includes('/graphql')),
+    'did not find any requests to /graphql'
+  );
 
-    await browser.close();
-    proc.kill();
-  },
+  await browser.close();
+  proc.kill();
   // $FlowFixMe - Need to add timeouts
-  60000
-);
+}, 60000);
